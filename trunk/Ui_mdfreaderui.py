71203c46cc8a77d9d555b0524f30b379025e8163
aymeric.rateau@gmail.com <aymeric.rateau@gmail.com@61a8afda-fa8b-b6b9-8ca7-b1101f7eb24d>
2013-02-06 20:27:44 +0000

    mdfconverter : keep in memory last path selected for next browse. Print list of merged file for chekcing mdfreader : improved robustness reading signal name in case pointer is empty Made compatible to python3 export of xlsx with not resampled data.

--- trunk/Ui_mdfreaderui.py
+++ trunk/Ui_mdfreaderui.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file '/home/ratal/workspace/mdfreader/mdfreaderui.ui'
 #
-# Created: Sat Oct 20 16:05:35 2012
-#      by: PyQt4 UI code generator 4.9.1
+# Created: Tue Feb  5 21:46:15 2013
+#      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
 
