072903bc5d5e27d1e31bc3bfa3412d88519b939b
aymeric.rateau@gmail.com <aymeric.rateau@gmail.com@61a8afda-fa8b-b6b9-8ca7-b1101f7eb24d>
2014-05-20 20:04:25 +0000

    Made mdf Veusz import plugin compliant to latest mdf reader development introducing mdf version 4

--- mdfForVeuszPlugin.py
+++ mdfForVeuszPlugin.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 18 nov. 2010
 
@@ -7,7 +8,7 @@ Veusz version confirmed : until 1.16
 
 Instructions :
 1. copy or link mdfForVeuszPlugin.py into the plugins directory of Veusz
-2. copy or link mdfreader.py in root directory of Veusz
+2. copy or link mdfreader.py in root or plugin directory of Veusz
 3. Go in Veusz ; Edit / Preferences / Plugins tab and add mdfForVeuszPlugins.py
 
 '''
@@ -41,7 +42,7 @@ class ImportPlugin( mdfinfo ):
     author = 'Aymeric Rateau'
     description = 'Import MDF files'
     promote_tab = 'MDF'
-    file_extensions = set()
+    file_extensions = set(['.dat', '.mf4', '.mdf'])
 
     def __init__( self ):
         """Override this to declare a list of input fields if required."""
@@ -53,15 +54,34 @@ class ImportPlugin( mdfinfo ):
         params is a ImportPluginParams object.
         Returns (text, okaytoimport)
         """
-
-        info = mdfinfo( params.filename )
-        f = ''
-        f += 'Time: ' + info['HDBlock']['Date'] + ' '
-        f += info['HDBlock']['Time'] + '\n'
-        f += 'Author: ' + info['HDBlock']['Author'] + '\n'
-        f += 'Organisation: ' + info['HDBlock']['Organization' ] + '\n'
-        f += 'Project Name: ' + info['HDBlock']['ProjectName'] + '\n'
-        f += 'Vehicle: ' + info['HDBlock']['Vehicle'] + '\n' + 'Channel List:\n'
+        
+        info = mdfinfo( fileName=params.filename )
+        
+        if info.mdfversion < 400:
+            f = ''
+            f += 'Time: ' + info['HDBlock']['Date'] + ' '
+            f += info['HDBlock']['Time'] + '\n'
+            f += 'Author: ' + info['HDBlock']['Author'] + '\n'
+            f += 'Organisation: ' + info['HDBlock']['Organization' ] + '\n'
+            f += 'Project Name: ' + info['HDBlock']['ProjectName'] + '\n'
+            f += 'Subject: ' + info['HDBlock']['Subject'] + '\n' + 'Channel List:\n'
+        else:
+            from time import gmtime, strftime
+            fileDateTime=gmtime(info['HDBlock']['hd_start_time_ns']/1000000000)
+            date=strftime('%Y-%m-%d', fileDateTime)
+            time=strftime('%H:%M:%S', fileDateTime)
+            f = ''
+            f += 'Date Time: ' + date + '  ' + time + '\n'
+            if 'Comment' in info['HDBlock']:
+                Comment = info['HDBlock']['Comment']
+                if 'author' in Comment:
+                    f += 'Author: ' + Comment['author'] + '\n'
+                if 'department' in Comment:
+                    f += 'Organisation: ' + Comment['department' ] + '\n'
+                if 'project' in Comment:
+                    f += 'Project Name: ' + Comment['project'] + '\n'
+                if 'subject' in Comment:
+                    f += 'Subject: ' + Comment['subject'] + '\n' + 'Channel List:\n'
         for channelName in info.listChannels():
             f += '   ' + channelName + '\n'
         return f, True
@@ -78,7 +98,7 @@ class MdfImportPlugin( ImportPlugin, mdf ):
 
     name = "MDFImport plugin"
     author = "Aymeric Rateau"
-    description = "Reads MDF files from INCA"
+    description = "Reads MDF files from INCA or CANAPE"
 
     def __init__( self ):
         ImportPlugin.__init__( self )
@@ -93,8 +113,8 @@ class MdfImportPlugin( ImportPlugin, mdf ):
         data = mdf( params.filename )
         data.resample( samplingTime = params.field_results['mult'] )
         List = []
-        for channelName in data.keys():
-            if len( data[channelName]['data'] ) > 0 and data[channelName]['data'].dtype != '|S1':
+        for channelName in list(data.keys()):
+            if len( data[channelName]['data'] ) > 0 and not data[channelName]['data'].dtype.kind in ['S', 'U']:
                 #print( data[channelName]['data'].dtype )
                 List.append( ImportDataset1D( channelName, data[channelName]['data'] ) )
         return List
