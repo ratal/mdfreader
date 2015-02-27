8f4c08daf4590d4e5ab748f52426b58071a4218c
aymeric.rateau@gmail.com <aymeric.rateau@gmail.com@61a8afda-fa8b-b6b9-8ca7-b1101f7eb24d>
2015-02-27 23:41:05 +0000

    mdfreader: improved unicode handling for several export methods

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    version='0.0.3',
+    version='0.0.5',
 
     description='A Measured Data Format file parser',
     long_description=long_description,
@@ -35,7 +35,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
