b20e417731d33ce775a31a64d5ac6f9aa5982193
William Skellenger <wskellenger@gmail.com>
2017-08-07 22:58:28 -0400

    bugfix: the byteoffset field was never used, bitoffset could be max 0xFFFF, after which you would not be able to add more channels without crashing.

--- mdfreader/mdf3reader.py
+++ mdfreader/mdf3reader.py
@@ -1227,6 +1227,7 @@ class mdf3(mdf_skeleton):
             recordNumberOfBits = 0
             preceedingChannel = None
             bitOffset = 0
+            byteOffset = 0
             writePointer(fid, pointers['CG'][dataGroup]['firstCN'], fid.tell())  # first channel bock pointer from CG
             for channel in self.masterChannelList[masterChannel]:
                 pointers['CN'][dataGroup][channel] = {}
@@ -1254,11 +1255,18 @@ class mdf3(mdf_skeleton):
                 # channel description
                 desc = self.getChannelDesc(channel)
                 writeChar(fid, desc, size=127)  # channel description
-                
+
+                #if bitOffset exceeds two byte limit, we start using the byte offset field
+                if (bitOffset > 0xFFFF):
+                    bitOffset -= 0x10000
+                    byteOffset += 8192
+
+                #print("working on {} at {} byteoffset {}".format(channel, bitOffset, byteOffset))
+
                 try:
                     fid.write(pack(UINT16, bitOffset))  # bit position
                 except:
-                    print("Could not pack data for channel {} as UINT16 with bitOffset={}".format(channel, bitOffset))
+                    print("Error: Could not pack data for channel {}, {}:{}".format(channel, bitOffset, byteOffset))
                 
                 data = self.getChannelData(channel)  # channel data
                 temp = data
@@ -1315,7 +1323,7 @@ class mdf3(mdf_skeleton):
                 pointers['CN'][dataGroup][channel]['longChannelName'] = fid.tell()
                 fid.write(pack(LINK, 0))  # pointer to long channel name
                 fid.write(pack(LINK, 0))  # pointer to channel display name
-                fid.write(pack(UINT16, 0))  # No Byte offset
+                fid.write(pack(UINT16, byteOffset))  # No Byte offset
 
                 # TXblock for long channel name
                 writePointer(fid, pointers['CN'][dataGroup][channel]['longChannelName'], fid.tell())
