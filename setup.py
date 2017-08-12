c20674efef0516490deb94dadad51fc1ce0b24e9
Aymeric Rateau <aymeric.rateau@gmail.com>
2017-08-12 07:01:39 +0100

    some pep8 and put back blosc

--- setup.py
+++ setup.py
@@ -5,29 +5,29 @@ from os import path
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-name='mdfreader'
+name = 'mdfreader'
 version = '0.2.6'
 
-description='A Measured Data Format file parser'
+description = 'A Measured Data Format file parser'
 
 here = path.abspath(path.dirname(__file__))
 # Get the long description from the relevant file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-long_description=long_description
+long_description = long_description
 
 # The project's main homepage.
-url='https://github.com/ratal/mdfreader'
+url = 'https://github.com/ratal/mdfreader'
 
 # Author details
-author='Aymeric Rateau'
-author_email='aymeric.rateau@gmail.com'
+author = 'Aymeric Rateau'
+author_email = 'aymeric.rateau@gmail.com'
 
 # Choose your license
-license='GPL3'
+license = 'GPL3'
 
 # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-classifiers=[
+classifiers = [
     # How mature is this project? Common values are
     #   3 - Alpha
     #   4 - Beta
@@ -53,22 +53,22 @@ classifiers=[
 ]
 
 # What does your project relate to?
-keywords='Parser MDF file'
+keywords = 'Parser MDF file'
 
 # You can just specify the packages manually here if your project is
 # simple. Or you can use find_packages().
-packages=find_packages(exclude=['contrib', 'docs', 'tests*'])
+packages = find_packages(exclude=['contrib', 'docs', 'tests*'])
 
 # List run-time dependencies here.  These will be installed by pip when your
 # project is installed. For an analysis of "install_requires" vs pip's
 # requirements files see:
 # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-install_requires=['numpy>=1.7', 'sympy', 'cython>=0.21', 'lxml', 'bcolz']
+install_requires = ['numpy>=1.7', 'sympy', 'cython>=0.21', 'lxml', 'bcolz', 'blosc']
 
 # List additional groups of dependencies here (e.g. development dependencies).
 # You can install these using the following syntax, for example:
 # $ pip install -e .[dev,test]
-extras_require={
+extras_require = {
     'export': ['scipy', 'h5py', 'xlwt', 'xlwt3', 'openpyxl', 'pandas'],
     'converter': ['PyQt4'],
     'experimental': ['bitarray'],
@@ -90,12 +90,13 @@ extras_require={
 # To provide executable scripts, use entry points in preference to the
 # "scripts" keyword. Entry points provide cross-platform support and allow
 # pip to create the appropriate form of executable for the target platform.
-entry_points={
-   'console_scripts': [
-       'mdfconverter=mdfconverter.mdfconverter:main',
-   ],
+entry_points = {
+    'console_scripts': [
+    'mdfconverter=mdfconverter.mdfconverter:main',
+    ],
 }
-ext_modules=cythonize(Extension('dataRead', ['dataRead.pyx'], include_dirs=[numpy.get_include()]))
+ext_modules = cythonize(Extension('dataRead', ['dataRead.pyx'],
+                        include_dirs=[numpy.get_include()]))
 
 try:
     setup(name=name, version=version, description=description, long_description=long_description,
@@ -105,7 +106,7 @@ try:
 except SystemExit:  # in case cython failed
     extras_require.pop('experimental')
     install_requires.pop(-1)  # removes cython requirement 
-    install_requires.append('bitarray') # replaces cython requirement by bitarray
+    install_requires.append('bitarray')  # replaces cython requirement by bitarray
     setup(name=name, version=version, description=description, long_description=long_description,
         url=url, author=author, author_email=author_email, license=license, classifiers=classifiers, 
         keywords=keywords, packages=packages, install_requires=install_requires, extras_require=extras_require, 
