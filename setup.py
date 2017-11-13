02cb51a961f82c39c0ccd5f1d2e218d188129b2f
Aymeric Rateau <aymeric.rateau@gmail.com>
2017-11-13 21:55:57 +0100

    changed version numbering to 3 digits for easier bug fix releases

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 name = 'mdfreader'
-version = '0.2.7'
+version = '2.7.1'
 
 description = 'A Measured Data Format file parser'
 
