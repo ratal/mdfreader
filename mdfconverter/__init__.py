e9266dce2ffbfd426924d8dc49e3daa9c7f15c7f
Aymeric Rateau <aymeric.rateau@gmail.com>
2018-06-29 21:48:00 +0200

    version 2.7.8

--- mdfconverter/__init__.py
+++ mdfconverter/__init__.py
@@ -12,4 +12,4 @@
 # along with this program.  If not, see http://www.gnu.org/licenses.
 #
 # ----------------------------------------------------------------------
-__version__ = "2.7.7"
+__version__ = "2.7.8"
