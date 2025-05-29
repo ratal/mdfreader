5a78c37bc9d40cb2f5662974cdfa51b96a55d48b
John Lamerton <john.lamerton@polestar.com>
2025-05-29 09:52:18 +0100

    Update mdf4reader.py
    Resolve issue with the case of 'x' in formulae

--- mdfreader/mdf4reader.py
+++ mdfreader/mdf4reader.py
@@ -2343,7 +2343,7 @@ def _formula_conversion(vector, formula):
     except:
         warn('Please install sympy to convert channel ')
     X = symbols('X')
-    expr = lambdify(X, formula, modules='numpy', dummify=False)
+    expr = lambdify(X, formula.upper(), modules='numpy', dummify=False)
     return expr(vector)
 
 
